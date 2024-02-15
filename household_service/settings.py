cfd30ae0faf133d9e1af4bff9015fb4a4a3ff5e1
Rejwana7 <rejwanatabassum87871@gmail.com>
2024-02-15 17:48:48 +0600

    media file change

--- household_service/settings.py
+++ household_service/settings.py
@@ -145,6 +145,7 @@ STATICFILES_DIRS=[
 ]
 # Base url to serve media files
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
